8362eb38b5d8e7d9ec3ff32d2a46e28c3a7e5a4f
liwenbo <liwenbo.mail@foxmail.com>
2021-01-28 11:57:52 +0800

    List 方法操作test

---
+++ src/test/java/com/wei/demo/collection/Listtest.java
@@ -0,0 +1,41 @@
+package com.wei.demo.collection;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @Title Listtest
+ * @Author wei
+ * @Date 2021/1/28 11:02
+ * @Description
+ */
+@SpringBootTest
+public class Listtest {
+    private static final Logger logger = LoggerFactory.getLogger(Listtest.class);
+
+    @Test
+    public void testadd() {
+        List list = new ArrayList();
+//      add 添加内容 add(String,int),指定下标添加内容
+        list.add("demo1");
+        list.add("demo2");
+        list.add("demo3");
+        list.add("demo2");
+//      indexOf目标第一次出现下标
+        int demo2 = list.indexOf("demo2");
+        logger.info("demo位置:"+ demo2);
+//      subList查询区间范围内元素
+        List list1 = list.subList(1, 2);
+        logger.info("返回区间元素包左不包右"+list1);
+//      removeAll删除list所有元素
+        list.removeAll(list);
+        logger.info("返回list"+list);
+    }
+}
+
